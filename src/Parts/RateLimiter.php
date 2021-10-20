71e6bc11bbbb3757e06e555f0898c0580fc7a771
Ilya <irexont@gmail.com>
2021-10-20 16:39:52 +0300

    + rateLimiter with per second param

--- src/Parts/RateLimiter.php
+++ src/Parts/RateLimiter.php
@@ -10,16 +10,17 @@ class RateLimiter extends RedisPart
      * @param string $method
      * @param int $user_id
      * @param int $limit
+     * @param int $per_seconds
      * @return bool
      */
-    public function isLimitReached(string $method, int $user_id, int $limit): bool
+    public function isLimitReached(string $method, int $user_id, int $limit, int $per_seconds = 60): bool
     {
         $current_time = time();
-        $time_window = $current_time - ($current_time % 60);
+        $time_window = $current_time - ($current_time % $per_seconds);
         $key = sprintf('api_%s_%d_%d', $method, $user_id, $time_window);
         $count = $this->redis->incr($key);
 
-        $this->redis->expire($key, 60);
+        $this->redis->expire($key, $per_seconds);
 
         return $count > $limit;
     }
