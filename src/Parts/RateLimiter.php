05fe3218c1825702b8c71d0d3ef5764bec575cb0
Ilya <irexont@gmail.com>
2021-10-20 16:42:22 +0300

    + rateLimiter with custom key

--- src/Parts/RateLimiter.php
+++ src/Parts/RateLimiter.php
@@ -7,17 +7,16 @@ class RateLimiter extends RedisPart
     /**
      * Incr and check limit for key
      *
-     * @param string $method
-     * @param int $user_id
+     * @param string $key
      * @param int $limit
      * @param int $per_seconds
      * @return bool
      */
-    public function isLimitReached(string $method, int $user_id, int $limit, int $per_seconds = 60): bool
+    public function isLimitReached(string $key, int $limit, int $per_seconds = 60): bool
     {
         $current_time = time();
         $time_window = $current_time - ($current_time % $per_seconds);
-        $key = sprintf('api_%s_%d_%d', $method, $user_id, $time_window);
+        $key = sprintf('api_%s_%d', $key, $time_window);
         $count = $this->redis->incr($key);
 
         $this->redis->expire($key, $per_seconds);
